647a83336a3d25df6de9ff1b2e4f124d3dc8b067
Naki1273 <skzaa103@gmail.com>
2024-06-27 11:36:22 +0700

    God

---
+++ Godsd.py
@@ -0,0 +1,26 @@
+#name = "Cid Kagenou"
+#name1 = "Alpha"
+#print (name)
+#print (name1)
+
+#x1 = 89 % 6 #Mod การหารเอาเศษเหลือ
+#x2 = 2 ** 4 #การยกกำลัง
+#x3 = 100 // 9 #หารโดยไม่เอาเศษ หรือปัดเศษทิ้ง
+
+#print(x1)
+#print(x2)
+#print(x3)
+
+x1 = 89 % 6
+# x1 = 5
+
+x1 += 5
+# x1 = x1 + 5 
+x3 = x2 = x1
+# x1 x2 x3 = 10
+x2 /= 2
+# x2 / 2
+# x2 = 5
+print (x1)
+print (x2)
+print (x3)
---
+++ hello.py
@@ -0,0 +1,5 @@
+#คำสั่งแสดงข้อความ Ctrl+/
+print ("Hello World")
+
+name = "Tichanon Soisri"
+print(name)
