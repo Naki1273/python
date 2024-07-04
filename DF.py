b112aa4019eec16cb858f580692174130d120cde
Naki1273 <skzaa103@gmail.com>
2024-07-04 12:03:14 +0700

    test.3

---
+++ DF.py
@@ -0,0 +1,46 @@
+# i = 1
+
+# while i <= 100:
+#     if i % 2 == 1:
+#         print(i)
+#     i += 1
+    
+# friend = ["earn","puck","nicky","toey","beer","k้oง"]
+# for x in friend:
+#     print(x)
+
+# def addvolumn(num,num1):
+#     print ("เพิ่มเสียง")
+#     print (num)
+#     print (num1)
+
+# def delvolumn():
+#     print ("ลดเสียง")
+
+# def turnoffTV():
+#     print ("ปิดทีวี")
+
+# turnoffTV()    
+# addvolumn(2,3)
+# delvolumn()
+
+def plus(num,num2):
+    print (num + num2)
+    
+
+def delete(num3,num4):
+    print (num3 - num4)
+    
+
+def multiply(num5,num6):
+    print (num5 * num6)
+    
+
+def divide(num7,num8):
+    print (num7 / num8)
+    
+plus(10,5)
+delete(5,2)
+multiply(2,2)
+divide(4,2)
+
---
+++ GHF.py
@@ -0,0 +1,25 @@
+#a =50
+#b =50
+
+#if b >= a: 
+#    print("ผู้สูงอายุ")
+
+# money = (int(input("price")))
+# if money == 100:
+#     print("pepsi")
+# elif money == 20:
+#     print("coke")
+# elif money == 40:
+#     print("lay")
+
+i = 1
+
+while i <= 10:
+ if i % 2 == 0:
+    print(i)
+    i += 1
+
+
+
+
+
